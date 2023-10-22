bcc156ea577d86cd04a1009cbaf151dc218a6007
Medeni Aba <medeniabax@gmail.com>
2023-10-22 11:59:38 +0300

    Proje dosyası ekle.

---
+++ Giris2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giris2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string ad;
+            Console.Write("İsminizi yazın ");
+            ad = Console.ReadLine();
+            Console.WriteLine("Adınız : " + ad );
+
+
+
+            Console.Write("Bir sayı giriniz : ");
+            int x; 
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("girilen sayının karesi = " + x * x);
+            Console.Read();
+        }
+    }
+}
